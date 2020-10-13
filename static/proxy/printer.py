d02cd10072a74a8be9675daa0f0962b3359d6132
piyo02 <alzidni2000@gmail.com>
2020-10-13 16:10:17 +0800

    push

--- static/proxy/printer.py
+++ static/proxy/printer.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import cups
 from flask_cors import CORS
 import os
+import logging
+_logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/dotmatrix/print', methods=['POST'])
 def dotmatrix_print():
+    _logger.warning(1)
+    conn = cups.Connection()
+    printers = conn.getPrinters()
+    for printer in printers:
+        pass
     printer_data = request.form['printer_data']
     os.system('echo "%s" | lpr -l' % (printer_data, ))
     return jsonify({'status': 'OK'})
